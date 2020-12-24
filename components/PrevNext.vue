32e8878690f12dfc5e12d24536ac7f6e471dd76b
rubennaatje <ruben.soerdien@hotmail.com>
2020-12-25 00:48:43 +0100

    Update PrevNext.vue

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-between">
     <NuxtLink
       v-if="prev"
-      :to="{ name: 'blog-slug', params: { slug: prev.slug } }"
+      :to="{ name: 'results', params: { slug: prev.slug } }"
       class="text-primary font-bold hover:underline"
     >
       {{ prev.slug }}
@@ -10,7 +10,7 @@
     <span v-else>&nbsp;</span>
     <NuxtLink
       v-if="next"
-      :to="{ name: 'blog-slug', params: { slug: next.slug } }"
+      :to="{ name: 'results', params: { slug: next.slug } }"
       class="font-bold hover:underline"
     >
       {{ next.slug }}
