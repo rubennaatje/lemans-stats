a71ae4d302d9ec02bb5c61a102f8ab0d42a3a291
Unknown <ruben.soerdien@hotmail.com>
2020-12-26 15:02:17 +0100

    fix blog image bug

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -8,30 +8,32 @@
         :key="article.slug"
         class="pv4 bt bb b--black-10 ph3 ph0-l"
       >
-        <div class="flex flex-column flex-row-ns">
-          <div class="w-100 w-60-ns pr3-ns order-2 order-1-ns">
-            <h1 class="f3 athelas mt0 lh-title">
-              {{ article.title }}
-            </h1>
-            <p class="f5 f4-l lh-copy athelas">
-              {{ article.description }}
-            </p>
+        <nuxt-link :to="{ name: 'blog-slug', params: { slug: article.slug } }">
+          <div class="flex flex-column flex-row-ns">
+            <div class="w-100 w-60-ns pr3-ns order-2 order-1-ns">
+              <h1 class="f3 athelas mt0 lh-title">
+                {{ article.title }}
+              </h1>
+              <p class="f5 f4-l lh-copy athelas">
+                {{ article.description }}
+              </p>
+            </div>
+            <div class="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
+              <img
+                :src="article.img"
+                class="db"
+                alt="Photo of a dimly lit room with a computer interface terminal."
+              />
+            </div>
           </div>
-          <div class="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
-            <img
-              :src="'lemans-stats/' + article.img"
-              class="db"
-              alt="Photo of a dimly lit room with a computer interface terminal."
-            />
-          </div>
-        </div>
-        <p class="f6 lh-copy gray mv0">
-          By
-          <span class="ttu">{{
-            article.author ? article.author.name : 'unknown'
-          }}</span>
-        </p>
-        <time class="f6 db gray">{{ formatDate(article.updatedAt) }}</time>
+          <p class="f6 lh-copy gray mv0">
+            By
+            <span class="ttu">{{
+              article.author ? article.author.name : 'unknown'
+            }}</span>
+          </p>
+          <time class="f6 db gray">{{ formatDate(article.updatedAt) }}</time>
+        </nuxt-link>
       </article>
     </section>
   </div>
